ff4644416fb6639dded0d3cc97078ce068d672d7
Fred Campos <mail2frede@gmail.com>
2018-05-16 06:47:50 -0300

    {WIP}: creating graphs data structure

--- c11/algorithms/structures/graphs.c
+++ c11/algorithms/structures/graphs.c
@@ -1,29 +1,12 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <strings.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 typedef struct graph {
-  int vertices;
-  int vertoces;
-  int vertaces;
-  int edges;
+    int vertexes;
+    int edges;
 } Graph;
 
 
-/**
- * other func
- * @param int a
- * @param float b
- *
- */
-int other(int a, float b)
-{
-  return (float) a * b;
-}
-
-
 int main() {
-  Graph gr;
-  gr.vertices = 1;
-  other(1, 1.00);
+    Graph gr;
 }
