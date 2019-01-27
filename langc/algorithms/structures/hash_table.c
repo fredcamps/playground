3a89c45a86d99714c5d90d3dd84360ebf2549d0a
Fred Campos <mail2frede@gmail.com>
2019-01-27 10:32:14 -0200

    improved hash table functions

--- langc/algorithms/structures/hash_table.c
+++ langc/algorithms/structures/hash_table.c
@@ -1,18 +1,15 @@
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-/**
- * WIP: Implementing hash table
- */
 const int SIZE = 20;
 
-typedef struct node {
+typedef struct node_t {
     int key;
     int value;
 } Node;
 
-
-typedef struct hash_table {
+typedef struct hash_table_t {
     Node *nodes[SIZE];
     void (*insert)(int key, int value);
     void (*delete)(int key);
@@ -22,11 +19,13 @@ typedef struct hash_table {
 
 HashTable *hash_table;
 
-int hash_code(int key) {
+int hash_code(int key)
+{
     return key % SIZE;
 }
 
-void hash_table_insert(int key, int value) {
+void hash_table_insert(int key, int value)
+{
     int hash_index = hash_code(key);
     Node *node = (Node *) malloc(sizeof(Node));
     node->key = key;
@@ -40,10 +39,9 @@ void hash_table_insert(int key, int value) {
     hash_table->nodes[hash_index] = node;
 }
 
-Node *hash_table_get(int key) {
+Node *hash_table_get(int key)
+{
     int hash_index = hash_code(key);
-
-
     while (hash_table->nodes[hash_index] != NULL) {
         if (hash_table->nodes[hash_index]->key == key) {
             return hash_table->nodes[hash_index];
@@ -53,15 +51,39 @@ Node *hash_table_get(int key) {
     return NULL;
 }
 
-void hash_table_delete(int key) {
+void hash_table_delete(int key)
+{
+    //get the hash
+    int hash_index = hash_code(key);
 
+    Node *dummy_item = (Node*) malloc(sizeof(Node));
+    dummy_item->value = -1;
+    dummy_item->key = -1;
+
+    //move in array until an empty
+    while (hash_table->nodes[hash_index] !=NULL) {
+        if(hash_table->nodes[hash_index]->key == key) {
+            // Node* temp = hash_table->nodes[hash_index];
+            //assign a dummy item at deleted position
+            hash_table->nodes[hash_index] = dummy_item;
+            return;
+        }
+
+        //go to next cell
+        ++hash_index;
+
+        //wrap around the table
+        hash_index %= SIZE;
+    }
 }
 
-void hash_table_destroy() {
-
+void hash_table_destroy()
+{
+    free(hash_table);
 }
 
-HashTable *make_hash_table () {
+HashTable *make_hash_table ()
+{
     hash_table = (HashTable *) malloc(sizeof(HashTable));
     hash_table->insert = hash_table_insert;
     hash_table->get = hash_table_get;
@@ -73,9 +95,13 @@ HashTable *make_hash_table () {
 
 int main(int argc, char *argv[])
 {
-    HashTable *dict = make_hash_table();
-    dict->insert(1, 20);
-    dict->insert(2, 30);
-    dict->insert(4, 40);
+    HashTable *h = make_hash_table();
+    h->insert(1, 20);
+    h->insert(2, 30);
+    h->insert(4, 40);
+    Node *temp = h->get(2);
+    assert(temp->key == 2);
+    printf("%d", temp->key);
+    h->destroy();
     return 0;
 }
