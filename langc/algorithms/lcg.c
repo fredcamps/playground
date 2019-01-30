6ff67ca6af4d5941d8e2b19183f9f7caa14063b4
Fred Campos <mail2frede@gmail.com>
2019-01-30 10:21:15 -0200

    fixed indent (untabify) lcg.c

--- langc/algorithms/lcg.c
+++ langc/algorithms/lcg.c
@@ -10,7 +10,7 @@ int rseed = 0;
 
 inline void srand(int x)
 {
-	rseed = x;
+    rseed = x;
 }
 
 #ifndef MS_RAND
@@ -18,7 +18,7 @@ inline void srand(int x)
 
 inline int rand()
 {
-	return rseed = (rseed * 1103515245 + 12345) & RAND_MAX;
+    return rseed = (rseed * 1103515245 + 12345) & RAND_MAX;
 }
 
 #else /* MS rand */
@@ -28,18 +28,18 @@ inline int rand()
 
 inline int rand()
 {
-	return (rseed = (rseed * 214013 + 2531011) & RAND_MAX_32) >> 16;
+    return (rseed = (rseed * 214013 + 2531011) & RAND_MAX_32) >> 16;
 }
 
 #endif/* MS_RAND */
 
 int main()
 {
-	int i;
-	printf("rand max is %d\n", RAND_MAX);
+    int i;
+    printf("rand max is %d\n", RAND_MAX);
 
-	for (i = 0; i < 100; i++)
-		printf("%d\n", rand());
+    for (i = 0; i < 100; i++)
+        printf("%d\n", rand());
 
-	return 0;
+    return 0;
 }
