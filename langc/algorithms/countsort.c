d3d171f932b30b8b00d870531abcbcc32e899070
Fred Campos <mail2frede@gmail.com>
2019-01-30 10:24:22 -0200

    fixed indent (untabify) countsort.c

--- langc/algorithms/countsort.c
+++ langc/algorithms/countsort.c
@@ -8,48 +8,48 @@
 */
 void count_sort(int arr[], int n, int k)
 {
-	// create an integer array of size n to store sorted array
-	int output[n];
+    // create an integer array of size n to store sorted array
+    int output[n];
 
-	// create an integer array of size k, initialized by all zero
-	int freq[k];
-	memset(freq, 0, sizeof(freq));
+    // create an integer array of size k, initialized by all zero
+    int freq[k];
+    memset(freq, 0, sizeof(freq));
 
-	// using value of integer in the input array as index,
-	// store count of each integer in freq[] array
-	for (int i = 0; i < n; i++)
-		freq[arr[i]]++;
+    // using value of integer in the input array as index,
+    // store count of each integer in freq[] array
+    for (int i = 0; i < n; i++)
+        freq[arr[i]]++;
 
-	// calculate the starting index for each integer
-	int total = 0;
-	for (int i = 0; i < k; i++)
-	{
-		int old_count = freq[i];
-		freq[i] = total;
-		total += old_count;
-	}
+    // calculate the starting index for each integer
+    int total = 0;
+    for (int i = 0; i < k; i++)
+    {
+        int old_count = freq[i];
+        freq[i] = total;
+        total += old_count;
+    }
 
-	// copy to output array, preserving order of inputs with equal keys
-	for (int i = 0; i < n; i++)
-	{
-		output[freq[arr[i]]] = arr[i];
-		freq[arr[i]]++;
-	}
+    // copy to output array, preserving order of inputs with equal keys
+    for (int i = 0; i < n; i++)
+    {
+        output[freq[arr[i]]] = arr[i];
+        freq[arr[i]]++;
+    }
 
-	// copy the output array back to the input array
-	for (int i = 0; i < n; i++)
-		arr[i] = output[i];
+    // copy the output array back to the input array
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
 }
 
 // C program for stable version of counting sort
 int main()
 {
-	int arr[] = { 4, 2, 10, 10, 1, 4, 2, 1, 10 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	count_sort(arr, n, 11);
+    int arr[] = { 4, 2, 10, 10, 1, 4, 2, 1, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    count_sort(arr, n, 11);
 
-	for (int i = 0; i < n; i++)
-		printf("%d ", arr[i]);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
 
-	return 0;
+    return 0;
 }
