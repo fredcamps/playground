841921d3b40ecb63003a26da4bc33eb6e1fc921a
Fred Campos <mail2frede@gmail.com>
2019-08-02 16:14:48 -0300

    pthreads example working

--- langc/sysdev/threads.c
+++ langc/sysdev/threads.c
@@ -2,8 +2,30 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void thread_first(void *args);
+void *thread_first(void *args)
+{
+    int *id = (int *) args;
+    printf("\nThread number %d \n", *id);
+}
 
-void thread_second(void *args);
+int main(int argc, char *argv[])
+{
 
-void thread_third(void *args);
+    int number_of_threads = 10;
+    pthread_t threads[number_of_threads];
+    int t_args[number_of_threads];
+
+    printf("Before create threads\n");
+    for (int i = 0; i < number_of_threads; ++i) {
+        t_args[i] = i;
+        pthread_create(&threads[i], NULL, thread_first, (void *) &t_args[i]);
+    }
+
+    for (int j = 0; j < number_of_threads; ++j) {
+        pthread_join(threads[j], NULL);
+    }
+    printf("After threads joined\n");
+    pthread_exit(NULL);
+
+    return 0;
+}
